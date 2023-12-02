351e24cf80aba652d396950bec7d723aeba8b9f4
Anggun <barokatillah16@gmail.com>
2023-12-02 14:14:11 +0700

    tampilkan dan batalkan

--- tugas1/fix.py
+++ tugas1/fix.py
@@ -3,6 +3,24 @@ def simpan_data(acara):
         for acara_peserta in acara.acara_peserta:
             file.write(f"{acara_peserta[0]}, {acara_peserta[1]}\n")
 
+def tampilkan_daftar_peserta(self):
+    for i, (acara, peserta) in enumerate (self.acara_peserta, start=1):
+        print(f'{i}.{peserta}. {acara}')
+    input("Press any key to continue..")
+    os.system("cls")
+
+def batalkan_pendaftaran(self,nama_peserta):
+    ketemu = False
+    for acara_peserta in self.acara_peserta[:]:
+        if acara_peserta[1] == nama_peserta:
+            self.acara_peserta.remove(acara_peserta)
+            print(f"{nama_peserta} berhasil membatalkan pendaftaran.")
+            ketemu = True
+        if not ketemu:
+            print(f"{nama_peserta} tidak terdaftar untuk acara apapun.")
+        input('Press Enter to continue...')
+        os.system('cls')
+
 
 def baca_data():
     acara = Acara()
