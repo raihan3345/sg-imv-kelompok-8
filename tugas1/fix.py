43b1edeae0b7c7c910db572663422c1d006efa2f
Raihan Radhitya Setiawan <59205006+raihan3345@users.noreply.github.com>
2023-12-03 16:06:49 +0700

    fix some bug and change main menu to look more attractive

--- tugas1/fix.py
+++ tugas1/fix.py
@@ -9,29 +9,29 @@ class Acara:
         os.system('cls')
 
     def daftar_peserta(self, nama_peserta, nama_acara):
-        self.acara_peserta.append(nama_peserta, nama_acara)
-        print(f"{nama_peserta} berhasil mendaftar untuk {self.nama_acara}.")
+        self.acara_peserta.append((nama_peserta, nama_acara))
+        print(f"{nama_peserta} berhasil mendaftar untuk {nama_acara}.")
         self.clear()
 
     def cari_peserta(self, nama_peserta):
-        peserta_terdaftar = [(acara, peserta) for acara, peserta in self.acara_peserta if peserta == nama_peserta]
+        peserta_terdaftar = [(peserta, acara) for peserta, acara in self.acara_peserta if peserta == nama_peserta]
         if peserta_terdaftar:
-            for acara, peserta in peserta_terdaftar:
+            for peserta, acara in peserta_terdaftar:
                 print(f"{peserta} terdaftar untuk {acara}.")
         else:
             print(f"{nama_peserta} tidak terdaftar untuk acara apapun.")
         self.clear()
     
     def tampilkan_daftar_peserta(self):
-        for i, (acara, peserta) in enumerate (self.acara_peserta, start=1):
-            print(f'{i}.{peserta}. {acara}')
+        for i, (peserta, acara) in enumerate (self.acara_peserta, start=1):
+            print(f'{i}. {peserta} - {acara}')
         self.clear()
 
     def batalkan_pendaftaran(self,nama_peserta):
         ketemu = False
-        for acara_peserta in self.acara_peserta[:]:
-            if acara_peserta[1] == nama_peserta:
-                self.acara_peserta.remove(acara_peserta)
+        for nama in self.acara_peserta[:]:
+            if nama[0] == nama_peserta:
+                self.acara_peserta.remove(nama)
                 print(f"{nama_peserta} berhasil membatalkan pendaftaran.")
                 ketemu = True
             if not ketemu:
@@ -49,8 +49,78 @@ def baca_data():
         with open("data_pendaftaran.txt", "r") as file:
             baca_file = file.readlines()
             for peserta in baca_file:
-                nama_acara, nama_peserta = peserta.strip().split(", ")
-                acara.acara_peserta.append((nama_acara, nama_peserta))
+                nama_peserta, nama_acara = peserta.strip().split(", ")
+                acara.acara_peserta.append((nama_peserta, nama_acara))
     except FileNotFoundError:
         pass
     return acara
+
+acara = baca_data()
+while True:
+    print('\n' + '='*30)
+    print('    🎉 PENDAFTARAN EVENT 🎉')
+    print('='*30 + '\n')
+    print("1. 📝 Daftar Peserta")
+    print("2. 🔍 Cari Peserta")
+    print("3. ❌ Batalkan Pendaftaran")
+    print("4. 📋 Tampilkan Daftar Peserta")
+    print("0. 🚪 Keluar\n")
+
+    pilihan = input("👉 Masukkan pilihan Anda: ")
+
+    if pilihan == "1":
+        os.system('cls')
+        print('\n' + '='*30)
+        print('\tPENDAFTARAN EVENT')
+        print('='*30)
+        nama_peserta = input('\n👤 Masukkan Nama Anda: ')
+        print('🎯 Pilih Acara yang Ingin Anda Ikuti:')
+        print('1. Study Group IMV')
+        print('2. Workshop Deep Learning')
+        print('3. Workshop Image Detection')
+        print('4. Workshop Image Optimization')
+        pilihan = input('Masukkan nomor pilihan Anda: ')
+        if pilihan == '1':
+            nama_acara = 'Study Group IMV'
+        elif pilihan == '2':
+            nama_acara = 'Workshop Deep Learning'
+        elif pilihan == '3':
+            nama_acara = 'Workshop Image Detection'
+        elif pilihan == '4':
+            nama_acara = 'Workshop Image Optimization'
+        else:
+            print('Pilihan tidak ada.')
+            continue
+        acara.daftar_peserta(nama_peserta, nama_acara)
+
+    elif pilihan == "2":
+        os.system('cls')
+        print('\n' + '='*30)
+        print('      🔍 CARI PESERTA 🔍')
+        print('='*30)
+        nama_peserta = input("\n👤 Masukkan Nama Peserta: ")
+        acara.cari_peserta(nama_peserta)
+
+    elif pilihan == "3":
+        os.system('cls')
+        print('\n' + '='*30)
+        print(' ❌ PEMBATALAN PENDAFTARAN ❌')
+        print('='*30)
+        nama_peserta = input("\n👤 Masukkan Nama Peserta: ")
+        acara.batalkan_pendaftaran(nama_peserta)
+
+    elif pilihan == "4":
+        os.system('cls')
+        print('\n' + '='*30)
+        print('\tLIST PESERTA')
+        print('='*30)
+        acara.tampilkan_daftar_peserta()
+
+    elif pilihan == "0":
+        simpan_data(acara)
+        print("Terima kasih, Telah Mendaftar!!")
+        break
+
+    else:
+        print("Pilihan Tidak Valid. (masukkan antara 0 - 1)")
+        input('Press Enter to continue...')
